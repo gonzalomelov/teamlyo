cd418bb89a829ed199f4a43e02922af13d7a2f7f
Gonzalo melo <gonzalomelov@gmail.com>
2015-04-11 18:23:29 -0300

    Add home controller

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var controller = require('controllers/home.js');
+
 /* GET main page. */
-router.get('/', function(req, res, next) {
-    res.render('home', { title: 'teamlyo' });
-});
+router.get('/', controller.getSets);
 
 module.exports = router;
