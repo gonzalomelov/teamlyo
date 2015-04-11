6f049aff2b6228f602a8520d00470039cc6c9642
Gonzalo melo <gonzalomelov@gmail.com>
2015-04-11 18:16:46 -0300

    Add home page

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET main page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'sety' });
+    res.render('home', { title: 'teamlyo' });
 });
 
 module.exports = router;
